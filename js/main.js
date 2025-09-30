22973d4bffbd4d2cf73dab8330da0e27f1ba1f2c
Kingyul Kim <youremail@example.com>
2025-09-30 16:28:56 +0900

    스크립트 수정

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
             const layer = `
                 <div id="${this_name}_layer_popup" class="my_popup_layer">
                     <div class="inner_wrap container">
-                        <iframe class="hide_scroll" src="../team_folder/${this_name}.html" style="width: 100%; height: 100%; overflow: hidden" ></iframe>
+                        <!--iframe class="hide_scroll" src="../team_folder/${this_name}.html" style="width: 100%; height: 100%; overflow: hidden" ></iframe-->
                     </div>
                 </div>
             `;
