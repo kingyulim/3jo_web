ed815341f8719d7adbf73879b8b410b719cda1a6
Kingyul Kim <youremail@example.com>
2025-09-30 17:23:34 +0900

    스크립트 수정2

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
             const layer = `
                 <div id="${this_name}_layer_popup" class="my_popup_layer">
                     <div class="inner_wrap container">
-                        <!--iframe class="hide_scroll" src="../team_folder/${this_name}.html" style="width: 100%; height: 100%; overflow: hidden" ></iframe-->
+                        <iframe class="hide_scroll" src="../team_folder/${this_name}.html" style="width: 100%; height: 100%; padding: 16px" ></iframe>
                     </div>
                 </div>
             `;
@@ -23,8 +23,18 @@ $(document).ready(function(){
             $("body").append(layer);
         }
 
+        const my_popup_layer = $(".my_popup_layer");
 
-        $(".my_popup_layer > .inner_wrap").load("./team_folder/" + this_name + ".html", function(){
+        my_popup_layer.click(function(e){
+            const l_t = $(e.target);
+
+            if(l_t.is(my_popup_layer)){
+                my_popup_layer.remove();
+            }
+        });
+
+        /*
+        $(".my_popup_layer > .inner_wrap").load("./team_folder/" + this_name + ".html .container", function(){
             const my_popup_layer = $(".my_popup_layer");
 
             my_popup_layer.click(function(e){
@@ -35,5 +45,6 @@ $(document).ready(function(){
                 }
             });
         });
+        */
     });
 });
