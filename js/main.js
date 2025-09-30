546c3e062b906e432cd2158a7bc822ee3d1d2259
Kingyul Kim <youremail@example.com>
2025-09-30 15:50:14 +0900

    김규림이 수정함

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
             const layer = `
                 <div id="${this_name}_layer_popup" class="my_popup_layer">
                     <div class="inner_wrap container">
+                        <iframe class="hide_scroll" src="../team_folder/${this_name}.html" style="width: 100%; height: 100%; overflow: hidden" ></iframe>
                     </div>
                 </div>
             `;
@@ -24,16 +25,18 @@ $(document).ready(function(){
 
         /*
         $(".my_popup_layer > .inner_wrap").load("./team_folder/" + this_name + ".html", function(){
-            const my_popup_layer = $(".my_popup_layer");
-
-            my_popup_layer.click(function(e){
-                const l_t = $(e.target);
-
-                if(l_t.is(my_popup_layer)){
-                    my_popup_layer.remove();
-                }
-            })
+           
         });
         */
+
+        const my_popup_layer = $(".my_popup_layer");
+
+        my_popup_layer.click(function(e){
+            const l_t = $(e.target);
+
+            if(l_t.is(my_popup_layer)){
+                my_popup_layer.remove();
+            }
+        });
     });
 });
