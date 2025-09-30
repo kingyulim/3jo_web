e9ceb422ff65dab0de49f88270aeea7014a8dcab
Kingyul Kim <youremail@example.com>
2025-09-30 17:28:54 +0900

    스크립트 수정2

--- js/main.js
+++ js/main.js
@@ -2,49 +2,35 @@ $(document).ready(function(){
     const member_list_wrap = $("#member_list_wrap");
 
     member_list_wrap.on("click", "li", function(){
-        const t = $(this);
-        const this_name = t.data("name");
+        const this_name = $(this).data("name");
 
-        if(this_name == null){
+        if(!this_name){
             alert('데이터가 빈 값입니다.');
-
             return;
         }
 
-        if($(".my_popup_layer").length === 0){
-            const layer = `
-                <div id="${this_name}_layer_popup" class="my_popup_layer">
-                    <div class="inner_wrap container">
-                        <iframe class="hide_scroll" src="${window.location.href}/team_folder/${this_name}.html" style="width: 100%; height: 100%; padding: 16px" ></iframe>
-                    </div>
-                </div>
-            `;
+        // 이미 열려있으면 제거 후 다시 열기
+        $(".my_popup_layer").remove();
 
-            $("body").append(layer);
-        }
+        const layer = `
+            <div class="my_popup_layer">
+                <div class="inner_wrap container">
+                    <iframe class="hide_scroll"
+                            src="./team_folder/${this_name}.html">
+                    </iframe>
+                </div>
+            </div>
+        `;
+
+        $("body").append(layer);
 
         const my_popup_layer = $(".my_popup_layer");
 
-        my_popup_layer.click(function(e){
-            const l_t = $(e.target);
-
-            if(l_t.is(my_popup_layer)){
+        // 중복 이벤트 방지
+        my_popup_layer.off("click").on("click", function(e){
+            if($(e.target).is(my_popup_layer)){
                 my_popup_layer.remove();
             }
         });
-
-        /*
-        $(".my_popup_layer > .inner_wrap").load("./team_folder/" + this_name + ".html .container", function(){
-            const my_popup_layer = $(".my_popup_layer");
-
-            my_popup_layer.click(function(e){
-                const l_t = $(e.target);
-
-                if(l_t.is(my_popup_layer)){
-                    my_popup_layer.remove();
-                }
-            });
-        });
-        */
     });
 });
