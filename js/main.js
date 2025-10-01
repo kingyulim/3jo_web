42866ead5555e45cf4254e1909dc059079db05df
Kingyul Kim <youremail@example.com>
2025-10-01 11:18:37 +0900

    메인 인덱스 페이드 프론트 완료 202510011118

--- js/main.js
+++ js/main.js
@@ -1,33 +1,70 @@
 $(document).ready(function(){
     const member_list_wrap = $("#member_list_wrap");
 
-    member_list_wrap.on("click", "li", function(){
-        const this_name = $(this).data("name");
+    //
+    member_list_wrap.on("click mouseenter mouseleave", "li[data-name]", function(e){
+        const t = $(this);
+        const this_name = t.data("name");
 
-        if(!this_name){
-            alert('데이터가 빈 값입니다.');
+        switch(e.type){
+            case "click" : 
+               if(this_name == null || this_name === ""){
+                    alert("데이터가 빈 값입니다.");
+                    return;
+                }
 
-            return;
-        }
+                if($(".my_popup_layer").length > 0) return;
 
-        $(".my_popup_layer").remove();
+                const layer = `
+                    <div class="my_popup_layer">
+                        <div class="inner_wrap container" style="padding: 16px;">
+                            <iframe src="./team_folder/${this_name}.html" style="width: 100%; height: 100%; border: 0;"></iframe>
+                        </div>
+                    </div>
+                `;
 
-        const layer = `
-            <div class="my_popup_layer">
-                <div class="inner_wrap container">
-                    <iframe src="./team_folder/${this_name}.html" style="width: 100%; height: 100%; padding: 16px"></iframe>
-                </div>
-            </div>
-        `;
+                $("body").append(layer);
 
-        $("body").append(layer);
+                $(document).on("click", ".my_popup_layer", function(e){
+                    if ($(e.target).is(".my_popup_layer")) {
+                        $(this).remove();
+                    }
+                });
 
-        const my_popup_layer = $(".my_popup_layer");
+                break;
 
-        my_popup_layer.off("click").on("click", function(e){
-            if($(e.target).is(my_popup_layer)){
-                my_popup_layer.remove();
-            }
-        });
+            case "mouseenter" :
+                const img_item = t.find(".my_img").children();  
+                const item_count = img_item.length;
+
+                console.log(item_count);
+                
+                if(item_count <= 1) return;
+
+                let index = 0;
+
+                img_item.hide().eq(0).show();
+
+                fade_slide = setInterval(function(){
+                    const current = img_item.eq(index);
+                    index = (index + 1) % item_count;
+                    const next = img_item.eq(index);
+
+                    current.fadeOut(300, function() {
+                        next.fadeIn(300);
+                    });        
+                }, 2000);
+                
+
+                break;
+            
+            case "mouseleave" :
+                clearInterval(fade_slide);
+                fade_slide = null;
+
+                t.find(".my_img").children().hide().eq(0).show();    
+
+                break;
+        } 
     });
 });
